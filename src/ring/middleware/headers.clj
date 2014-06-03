84ff08569b604810d9b564f5c4acb2a941785c49
James Reeves <jreeves@weavejester.com>
2014-06-03 19:46:34 +0100

    Initial commit

---
+++ src/ring/middleware/headers.clj
@@ -0,0 +1,6 @@
+(ns ring.middleware.headers)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/ring/middleware/headers_test.clj
@@ -0,0 +1,7 @@
+(ns ring.middleware.headers-test
+  (:use clojure.test
+        ring.middleware.headers))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
