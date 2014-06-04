798d7670929eb31f19059ccdc6819149c9780134
James Reeves <jreeves@weavejester.com>
2014-06-04 20:25:17 +0100

    Add URL and description to project

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ring/ring-headers "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Ring middleware for common response headers"
+  :url "https://github.com/ring-clojure/ring-headers"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]
