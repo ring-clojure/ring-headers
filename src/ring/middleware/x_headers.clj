7eb58feb7de1d37796913fc05aec3af56696aed5
James Reeves <jreeves@weavejester.com>
2014-06-04 03:26:52 +0100

    Add docstring to wrap-frame-options

--- src/ring/middleware/x_headers.clj
+++ src/ring/middleware/x_headers.clj
@@ -1,4 +1,5 @@
 (ns ring.middleware.x-headers
+  "Middleware for adding various 'X-' response headers."
   (:require [clojure.string :as str]
             [ring.util.response :as resp]))
 
@@ -13,6 +14,21 @@
     (str/upper-case (name frame-options))))
 
 (defn wrap-frame-options
+  "Middleware that adds the X-Frame-Options header to the response. This governs
+  whether your site can be rendered in a <frame>, <iframe> or <object>, and is
+  typically used to prevent clickjacking attacks.
+
+  The following frame options are allowed:
+
+  :deny             - prevent any framing of the content
+  :sameorigin       - allow only the current site to frame the content
+  {:allow-from uri} - allow only the specified URI to frame the page
+
+  The :deny and :sameorigin options are keywords, while the :allow-from option
+  is a map consisting of one key/value pair.
+
+  Note that browser support for :allow-from is incomplete. See:
+  https://developer.mozilla.org/en-US/docs/Web/HTTP/X-Frame-Options"
   [handler frame-options]
   {:pre [(or (= frame-options :deny)
              (= frame-options :sameorigin)
