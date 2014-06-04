d18dbbe770b189a206fda5b9e58f5164402a3ce4
James Reeves <jreeves@weavejester.com>
2014-06-04 03:21:11 +0100

    Add wrap-frame-options middleware

--- src/ring/middleware/x_headers.clj
+++ src/ring/middleware/x_headers.clj
@@ -1,6 +1,23 @@
-(ns ring.middleware.x-headers)
+(ns ring.middleware.x-headers
+  (:require [clojure.string :as str]
+            [ring.util.response :as resp]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- allow-from? [frame-options]
+  (and (map? frame-options)
+       (= (keys frame-options) [:allow-from])
+       (string? (:allow-from frame-options))))
+
+(defn- format-frame-options [frame-options]
+  (if (map? frame-options)
+    (str "ALLOW-FROM " (:allow-from frame-options))
+    (str/upper-case (name frame-options))))
+
+(defn wrap-frame-options
+  [handler frame-options]
+  {:pre [(or (= frame-options :deny)
+             (= frame-options :sameorigin)
+             (allow-from? frame-options))]}
+  (let [header-value (format-frame-options frame-options)]
+    (fn [request]
+      (-> (handler request)
+          (resp/header "X-Frame-Options" header-value)))))
--- test/ring/middleware/x_headers_test.clj
+++ test/ring/middleware/x_headers_test.clj
@@ -1,7 +1,38 @@
 (ns ring.middleware.x-headers-test
   (:use clojure.test
-        ring.middleware.x-headers))
+        ring.middleware.x-headers
+        [ring.mock.request :only [request]]
+        [ring.util.response :only [redirect response content-type]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-wrap-frame-options
+  (let [handle-hello (constantly (response "hello"))]
+    (testing "deny"
+      (let [handler (wrap-frame-options handle-hello :deny)
+            resp    (handler (request :get "/"))]
+        (is (= (:headers resp) {"X-Frame-Options" "DENY"}))))
+
+    (testing "sameorigin"
+      (let [handler (wrap-frame-options handle-hello :sameorigin)
+            resp    (handler (request :get "/"))]
+        (is (= (:headers resp) {"X-Frame-Options" "SAMEORIGIN"}))))
+
+    (testing "allow-from"
+      (let [handler (wrap-frame-options handle-hello {:allow-from "http://example.com/"})
+            resp    (handler (request :get "/"))]
+        (is (= (:headers resp) {"X-Frame-Options" "ALLOW-FROM http://example.com/"}))))
+
+    (testing "bad arguments"
+      (is (thrown? AssertionError (wrap-frame-options handle-hello :foobar)))
+      (is (thrown? AssertionError (wrap-frame-options handle-hello {:allowfrom "foo"})))
+      (is (thrown? AssertionError (wrap-frame-options handle-hello {:allow-from nil}))))
+
+    (testing "response fields"
+      (let [handler (constantly
+                     (-> (response "hello")
+                         (content-type "text/plain")))
+            resp    ((wrap-frame-options handler :deny)
+                     (request :get "/"))]
+        (is (= resp {:status  200
+                     :headers {"X-Frame-Options" "DENY"
+                               "Content-Type" "text/plain"}
+                     :body    "hello"}))))))
