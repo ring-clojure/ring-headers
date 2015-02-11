efab4ec63a3fadcd45cdab2aa52a82e3c1cdfca8
James Reeves <jreeves@weavejester.com>
2015-02-11 21:14:56 +0000

    Update ring-core version to 1.3.2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.3.0"]]
+                 [ring/ring-core "1.3.2"]]
   :plugins [[codox "0.8.10"]]
   :codox {:project {:name "Ring-Headers"}}
   :profiles
