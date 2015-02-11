39496f7f49ed73af3a6d2faaa1e1c5977f2337ff
James Reeves <jreeves@weavejester.com>
2015-02-11 21:15:42 +0000

    Release 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring/ring-headers "0.1.1"
+(defproject ring/ring-headers "0.1.2"
   :description "Ring middleware for common response headers"
   :url "https://github.com/ring-clojure/ring-headers"
   :license {:name "The MIT License"
