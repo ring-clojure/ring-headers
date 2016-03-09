a94445269f17651aeb3bdcb5a875731b00ca3ff7
James Reeves <jreeves@weavejester.com>
2016-03-09 16:37:55 +0000

    Add test profiles for Clojure 1.7 and 1.8

--- project.clj
+++ project.clj
@@ -10,4 +10,6 @@
           :output-path "codox"}
   :profiles
   {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
+   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
