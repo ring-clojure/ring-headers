a7af10b6c1464ab320163cc2ab2d0039d7218747
James Reeves <jreeves@weavejester.com>
2016-03-09 16:37:12 +0000

    Update Ring dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,11 @@
   :url "https://github.com/ring-clojure/ring-headers"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring/ring-core "1.3.2"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-core "1.4.0"]]
   :plugins [[lein-codox "0.9.4"]]
   :codox {:project {:name "Ring-Headers"}
           :output-path "codox"}
   :profiles
-  {:dev {:dependencies [[ring/ring-mock "0.2.0"]]}
-   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-   :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
