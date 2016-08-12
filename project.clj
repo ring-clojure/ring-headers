7bb6182d2d287eab1f603cd44d90da7f75e6d2c6
James Reeves <jreeves@weavejester.com>
2016-08-12 18:15:32 +0100

    Update Codox plugin to 0.9.6

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.6.0-beta5"]]
-  :plugins [[lein-codox "0.9.4"]]
+  :plugins [[lein-codox "0.9.6"]]
   :codox {:project {:name "Ring-Headers"}
           :output-path "codox"}
   :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9" "test"]}
