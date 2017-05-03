78110392c06ce1e6189b60afd7cef64e5f888289
James Reeves <jreeves@weavejester.com>
2017-05-03 19:39:01 +0100

    Update Codox to 0.10.3

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring/ring-core "1.6.0"]]
-  :plugins [[lein-codox "0.9.6"]]
+  :plugins [[lein-codox "0.10.3"]]
   :codox {:project {:name "Ring-Headers"}
           :output-path "codox"}
   :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8:+1.9" "test"]}
